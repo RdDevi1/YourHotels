3c4906778329d9f896750d16a41d4cb731d11e27
Vitaly <anpilov.vitaly@gmail.com>
2023-12-21 13:56:11 +0300

    add colors and icons

---
+++ YourHotels/YourHotels/Extensions/UIColor+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  UIColor+Extension.swift
+//  YourHotels
+//
+//  Created by Vitaly Anpilov on 21.12.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static let hotelApp = HotelApp()
+}
+
+struct HotelApp {
+     let universalBlackPrimary = UIColor(named: "UniversalBlackPrimary")
+     let universalBlue = UIColor(named: "UniversalBlue")
+     let universalLightBlue = UIColor(named: "UniversalBlue")
+     let universalGray = UIColor(named: "UniversalGray")
+     let universalLightGray = UIColor(named: "UniversalLightGray")
+     let universalGrayBackground = UIColor(named: "UniversalGrayBackground")
+     let universalViewBackground = UIColor(named: "UniversalViewBackground")
+     let universalLightOrange = UIColor(named: "UniversalLightOrange")
+     let universalOrange = UIColor(named: "UniversalOrange")
+     let universalWhite = UIColor(named: "UniversalWhite")
+     let universalErrorColor = UIColor(named: "UniversalErrorColor")
+}
+
